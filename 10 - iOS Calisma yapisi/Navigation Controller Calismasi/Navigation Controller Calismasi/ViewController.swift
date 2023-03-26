2cd1a0fd6f8bc476cc02866af57db28d408a8802
oğuz yürken <oguz.yurken@gmail.com>
2023-03-26 20:54:32 +0300

    update

--- 10 - iOS Calisma yapisi/Navigation Controller Calismasi/Navigation Controller Calismasi/ViewController.swift
+++ 10 - iOS Calisma yapisi/Navigation Controller Calismasi/Navigation Controller Calismasi/ViewController.swift
@@ -11,9 +11,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+        //self.navigationItem.title = "Baslik Sa"
+        //self.navigationItem.prompt = "Aciklama Kod ile AS"
+        
+        //self.navigationItem.largeTitleDisplayMode = .never
+        
+        //let resim = UIImage(named: "navBaslik")
+        //self.navigationItem.titleView = UIImageView(image: resim)
     }
 
-
+    @IBAction func playTikla(_ sender: Any) {
+        print("Play tiklandi")
+    }
+    
+    @IBAction func kalpTikla(_ sender: Any) {
+     print("Kalp TIklandi")
+    }
+    
+    
+    @IBAction func segmentTiklandi(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0 :
+            print("First Tiklandi")
+        case 1 :
+            print("Second Tiklandi")
+        default:
+            break
+        }
+        
+    }
 }
 
