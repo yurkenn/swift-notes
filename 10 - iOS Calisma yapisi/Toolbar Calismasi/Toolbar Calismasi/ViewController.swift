5200ddc84fb715c8638c80c5b9307a3d57f25b77
oğuz yürken <oguz.yurken@gmail.com>
2023-03-28 18:49:27 +0300

    update

--- 10 - iOS Calisma yapisi/Toolbar Calismasi/Toolbar Calismasi/ViewController.swift
+++ 10 - iOS Calisma yapisi/Toolbar Calismasi/Toolbar Calismasi/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var etiket: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func geriAL(_ sender: Any) {
+        etiket.text = "geri al"
+    }
+    
+    @IBAction func oynat(_ sender: Any) {
+        etiket.text = "Oynat"
+    }
+    
+    @IBAction func homeButton(_ sender: Any) {
+        etiket.text = "Home"
+    }
 }
 
