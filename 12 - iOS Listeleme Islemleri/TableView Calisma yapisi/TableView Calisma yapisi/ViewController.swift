14bf1fc60390f98358cf1b4770bf4a6e546af63a
oğuz yürken <oguz.yurken@gmail.com>
2023-03-31 21:41:51 +0300

    update

---
+++ 12 - iOS Listeleme Islemleri/TableView Calisma yapisi/TableView Calisma yapisi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView Calisma yapisi
+//
+//  Created by Oğuz Yürken on 31.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
